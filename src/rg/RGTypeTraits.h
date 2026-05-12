32e248c82b7ee46a40a5b185b7fd5b619dbf9fe9
Seweryn Wasilewski <s.wasilewski@tenesys.pl>
2026-05-12 19:21:25 +0200

    refactor(rg): replace IResource vtable with typed GPU creation dispatch

--- src/rg/RGTypeTraits.h
+++ src/rg/RGTypeTraits.h
@@ -1,22 +1,17 @@
 #pragma once
 #include <concepts>
+#include <type_traits>
+
+struct IRHIBackend;
 
 template<typename T>
-concept Virtualizable = requires(T t) {
-  requires std::conjunction_v<std::is_default_constructible<T>, std::is_move_constructible<T>>;
-  typename T::Desc;
-  { t.create(typename T::Desc{}, (void*)nullptr) } -> std::same_as<void>;
-  { t.destroy(typename T::Desc{}, (void*)nullptr) } -> std::same_as<void>;
-};
+concept Virtualizable =
+  requires { typename T::Desc; } &&
+  std::is_default_constructible_v<T> &&
+  std::is_move_constructible_v<T> &&
+  requires(const typename T::Desc& d, IRHIBackend* b, void* h) {
+    { T::createGPU(d, b)     } -> std::same_as<void*>;
+    { T::destroyGPU(d, b, h) } -> std::same_as<void>;
+  };
 
 #define VIRTUALIZABLE_RESOURCE(T) Virtualizable T
-
-template<typename T>
-concept has_preRead = requires(T t) {
-  { t.preRead(typename T::Desc{}, (void*)nullptr) } -> std::same_as<void>;
-};
-
-template<typename T>
-concept has_preWrite = requires(T t) {
-  { t.preWrite(typename T::Desc{}, (void*)nullptr) } -> std::same_as<void>;
-};
